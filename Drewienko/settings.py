e410b646249ac78eebee282cdb2c4744fc7dc7de
tomek-oss <tomek6988@gmail.com>
2020-11-11 16:53:54 +0100

    first develop commit

--- Drewienko/settings.py
+++ Drewienko/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Drewienko.apps.DrewienkoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
