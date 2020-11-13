a0e2f5a96c0ff23380e5597faae8eb46c098cef3
MonikaSz <monika.ewa.szajwaj@gmail.com>
2020-11-13 22:09:53 +0100

    naprawa wyświetlania avatara

--- users/models.py
+++ users/models.py
@@ -12,7 +12,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     city = models.CharField("City", max_length=1024)
-    avatar = models.ImageField(upload_to='avatars', blank=True, default=None)
+    avatar = models.ImageField(upload_to='media_files/avatars', default="default-avatar.jpg")
 
     class Meta:
         verbose_name = "Użytkownik"
